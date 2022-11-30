ea92c5753023d78f2f56738b56de626ad0bcbf37
JulieVanHoudenhove <julie.van-houdenhove@etudiant.univ-reims.fr>
2022-11-30 23:18:14 +0100

    js panier, début tunnel achat

--- src/Controller/CompteController.php
+++ src/Controller/CompteController.php
@@ -11,8 +11,11 @@ class CompteController extends AbstractController
     #[Route('/compte', name: 'app_compte')]
     public function index(): Response
     {
+        $user=$this->getUser();
+        $commandes=$user->getCommandes;
+
         return $this->render('compte/index.html.twig', [
-            'controller_name' => 'CompteController',
+            'commandes' => $commandes,
         ]);
     }
 }
--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,8 +18,13 @@ class PanierController extends AbstractController
     }
 
     #[Route('/panier/validation', name: 'panier_validation')]
-    public function panierValidation(): Response
+    public function panierValidation(HttpFoundationRequest $request): Response
     {
+        $user = $this->getUser();
+        if ($user==null) {
+            return $this->redirectToRoute('app_login', ["panier"=>true]);
+        }
+        dd($request->server->get('HTTP_COOKIE'));
         return $this->render('panier/validation.html.twig', [
             'controller_name' => 'PanierController',
         ]);
