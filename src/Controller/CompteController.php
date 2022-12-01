46b207e0f2b4740376c162d6000e132a8ceddfff
JulieVanHoudenhove <julie.van-houdenhove@etudiant.univ-reims.fr>
2022-12-01 21:27:35 +0100

    tunnel d'achat + compte

--- src/Controller/CompteController.php
+++ src/Controller/CompteController.php
@@ -11,8 +11,11 @@ class CompteController extends AbstractController
     #[Route('/compte', name: 'app_compte')]
     public function index(): Response
     {
-        $user=$this->getUser();
-        $commandes=$user->getCommandes;
+        $user = $this->getUser();
+        if ($user==null) {
+            return $this->redirectToRoute('app_login', ["panier"=>true]);
+        }
+        $commandes=$user->getCommandes();
 
         return $this->render('compte/index.html.twig', [
             'commandes' => $commandes,
