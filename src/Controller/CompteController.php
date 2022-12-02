1a6cd3ca9e4483c5a14102dbf70d005af0c969ee
JulieVanHoudenhove <julie.van-houdenhove@etudiant.univ-reims.fr>
2022-12-02 23:48:30 +0100

    test pdf

--- src/Controller/CompteController.php
+++ src/Controller/CompteController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Commande;
+use PHPUnit\TextUI\Command;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Mpdf\Mpdf;
 
 class CompteController extends AbstractController
 {
@@ -19,6 +22,28 @@ class CompteController extends AbstractController
 
         return $this->render('compte/index.html.twig', [
             'commandes' => $commandes,
+        ]);
+    }
+
+    #[Route('/compte/pdf/{commande}', name: 'pdf')]
+    public function pdf(Commande $commande): Response
+    {
+        $mpdf = new \Mpdf\Mpdf();
+        $mpdf->useSubstitutions = false;
+        $mpdf->simpleTables = true;
+
+        $mpdf->SetFooter('Toulouse Culture');
+        $mpdf->WriteHTML($html);
+
+        $mpdf->SetTitle('Ticket commande'.$commande->getId());
+        $mpdf->SetAuthor('TC');
+        $mpdf->SetCreator('TC');
+        $mpdf->SetSubject('Validation de commande');
+
+        $mpdf->Output('Ticket commande'.$commande->getId());
+        return new JsonResponse([
+            'success' => true,
+            'data'    => []
         ]);
     }
 }
